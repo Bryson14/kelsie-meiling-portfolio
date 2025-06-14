eae1efeb9ae5a59b0dad5f36118d25daee7749c6
Bryson Meiling <bryson.meiling@gmail.com>
2025-06-14 08:18:18 -0700

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    image: {
+        service: {
+            entrypoint: "astro/assets/services/sharp",
+        },
+    },
+
+    compressHTML: true,
+
+    build: {
+        inlineStylesheets: "auto",
+    },
+    vite: {
+        plugins: [tailwindcss()],
+    },
+});
